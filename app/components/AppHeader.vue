78a7b5ff4768278249696889f8cade3d79baf6e7
Adejuwon Oshadipe <bibire95@gmail.com>
2026-08-17 21:09:40 +0100

    chore: update header logo asset

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex min-h-[72px] items-center border-b border-zinc-200 bg-white px-7 py-6 font-bold text-primary-800">
-    <div class="mr-1 grid size-6 place-items-center text-xl" aria-hidden="true">✦</div>
+    <img class="mr-1 size-6" src="/logo.svg" alt="">
     <span>Visa Wizard</span>
   </header>
 </template>
