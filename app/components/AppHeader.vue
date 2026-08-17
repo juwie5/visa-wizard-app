d20f937c2970362ed6e9d9efe2e106f0124732e9
Adejuwon Oshadipe <bibire95@gmail.com>
2026-08-17 21:16:05 +0100

    fix: bundle header logo asset

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import logoUrl from '~/assets/images/logo.svg'
+</script>
+
 <template>
   <header class="flex min-h-[72px] items-center border-b border-zinc-200 bg-white px-7 py-6 font-bold text-primary-800">
-    <img class="mr-1 size-6" src="/logo.svg" alt="">
+    <img class="mr-1 size-6" :src="logoUrl" alt="">
     <span>Visa Wizard</span>
   </header>
 </template>
